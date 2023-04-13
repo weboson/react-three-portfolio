1fa7cb068e1f7e505fb6db84c017b04123860ccc
Rishat <virisound@gmail.com>
2023-04-13 10:02:56 +0500

    PreLoader: before 3D model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -15,11 +15,29 @@ const Computers = () => {
   return (
     // сетка
     <mesh>
-      {/* свет */}
+      {/* //! 3 света: */ }
+      {/* //! 1-й сферичный с интенсивностью 0.15 */ }
       <hemisphereLight intensity={0.15} groundColor='black' />
+      {/* //! 2-ой точечный с интенсивностью 1 */}
       <pointLight intensity={1} />
+      {/* //! 3-й основной прожектор */}
+      <spotLight
+        position={[-20, 50, 10]}
+        angle={0.12}
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      
+      {/* //! сцена */}
       <primitive 
         object={computer.scene}
+        scale={0.75}
+        //! позиция (ниже заголовка) 
+        position={[0, -3.25, -1.5]}
+        //! камера смотрит чуть сверхку 
+        rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
   );
