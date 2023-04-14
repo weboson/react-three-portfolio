f7bebfe48e8455ac246c734ead06dcfa6de3022c
Rishat <virisound@gmail.com>
2023-04-15 00:29:54 +0500

    Animated Scrolling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,8 +26,29 @@ const Hero = () => {
           </p>
         </div>
       </div>
-
+{/*//! 3D desctop_pc */}
       <ComputersCanvas/>
+
+{/*//! scroll */}
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href="#about">
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            {/*//! анимация шарика с помощью библитеки "framer-motion" */}
+            <motion.dev
+              animate={{
+                y: [0, 24, 0]
+              }}
+              transition={{
+                duration: 1.5, // продолжительность
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              // бегающий шарик 
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
