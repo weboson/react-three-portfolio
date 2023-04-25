f9d2495a8e15a000b557cf96562190b271d9c27b
Rishat <virisound@gmail.com>
2023-04-26 04:39:46 +0500

    Section: Tech (balls)

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,24 @@
+//! раздел Tech
 import React from 'react'
+// компонент шарика (ball)
+import { BallCanvas } from './canvas';
+// обертка для стилей, анимаций...
+import { SectionWrapper } from '../hoc';
+// контент (текст) для шаров
+import { technologies } from '../constants';
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+//!
+export default SectionWrapper(Tech, '');
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,80 @@
+//! 3D ball
 import React from 'react'
+// Suspense (задержка) - это React-компонент, который позволяет ОТОБРАЖАТЬ ЗАПАСНОЙ ВАРИАНТ, 
+// ПОКА его дочерние элементы НЕ ЗАКОНЧАТ ЗАГРУЗКУ.
+//! Suspense даёт возможность компонентам «дождаться» чего-то перед рендером.
+import { Suspense } from 'react';
+// библиотека ускоряет 3D анимацию и позволяет объединить компоненты React с их jsx и синтаксис библиотеки “Threejs”
+import { Canvas } from '@react-three/fiber';
+// Это 3D-библиотека с различными функциями, помогающими рендерить 3D React Three Fiber
+//! decal - это наклейка на 3D модель, png из src\assets\tech импортировано src\constants\index.js и export переменными 
+// OrbitControls - возможность масштабирования и вращения камеры, 
+// Preload - перед рендером выполняет полную загрузку 3D модели, чтобы анимация была плавной, без рывков
+// useTexture - Удобный хук, использующий useLoader и TextureLoader.
+import {
+  Decal, 
+  Float, 
+  OrbitControls, 
+  Preload, 
+  useTexture
+} from '@react-three/drei';
+// наш компонент для индикатор загрузки 3D модели - использующий useProgress от '@react-three/drei'
+// индикатор загрузки
+import CanvasLoader from '../Loader';
 
-const Ball = () => {
+const Ball = (props) => {
+  // decal - наклейка на текстуру - пропсы из компонента снизу
+  const [decal] = useTexture([props.imgUrl])
+
+  // (всё должно быть, иначе -  ничего не видно)
+  // ambientLight - окружающий свет
+  // directionalLight - направленный свет
+  // mesh - сетка 
+  // castShadow - отбрасывать тень
+  // icosahedronGeometry - математическая фигня (автор сам не знает)
+  // meshStandardMaterial - чтобы покарсить 3D модель
+  // polygonOffset - смещение (в доках не нашел) 
+  // flatShading - плоское затенение (в доках не нашел) - чтобы тень была не круглая, а также УГЛОВАТАЯ, как и шары
   return (
-    <div>Ball</div>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} /> 
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1,1]} />
+        <meshStandardMaterial 
+          color='#fff8eb'
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal 
+          position={[0,0,1]}
+          // повернуть по горизонтали  (чтобы не было как в зеркале)
+          rotation={[ 2 * Math.PI, 0, 6.25]}
+          flatShading
+          map={decal}
+        />
+      </mesh>
+    </Float>
   )
 }
 
-export default Ball
+// холст для модели: камера, свет и т.д.
+const BallCanvas = ({ icon }) => {
+  return (
+    // пoхоже, как и в Computers.jsx
+    <Canvas 
+       frameloop='demand'
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader/>}>
+        <OrbitControls enableZoom={false} />
+          <Ball imgUrl={icon} />
+      </Suspense>
+
+      <Preload all/>
+    </Canvas>
+  )
+}
+
+export default BallCanvas
