e28cf3be23b1c35df7a2f25fba83b77307ae8061
Rishat <virisound@gmail.com>
2023-05-02 01:18:39 +0500

    Section: Testimonials

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,72 @@
+//! раздел "testimonials" (отзывы )
 import React from 'react'
 
+// библиотека анимаций, например при скролле блок растет в размерах
+import { motion } from 'framer-motion';
+// стили
+import { styles } from '../styles';
+// обертка (href, стили, анимация наезда из пустоты)
+import { SectionWrapper } from '../hoc';
+// стили в виде JS-функций возвращающих объекты со стилями -  для подсвечивания текста
+import { fadeIn, textVariant } from '../utils/motion';
+// данные (текст) для отзывов
+import { testimonials } from '../constants';
+
+// карты
+// компонент, который сразу возращает
+const FeedbackCard = ({ index, testimonial, name, designation, company, image }) => (
+  <motion.div 
+    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
+  >
+    <p className='text-white font-black text-[48px]'>"</p>
+    <div className='mt-1'>
+      <p className='text-white tracking-wider text-[18px]'>{testimonial}</p>
+      <div className='mt-7 flex justify-between items-center gap-1'>
+        <div flex-1 flex flex-col>
+          <p className='text-white font-medium text-[16px]'>
+            <span className='blue-text-gradient'>@</span> {name}
+          </p>
+          <p className='mt-1 text-secondary text-[12px]'>
+            {designation} of {company}
+          </p>
+        </div>
+        <img 
+          src={image} 
+          alt={`feedback-by-${name}`} 
+          className="w-10 h-10 rounded-full object-cover"
+        />
+      </div>
+    </div>
+  </motion.div>
+)
+
+// блоки
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div className='mt-12 bg-black-100 rounded-[20px]'>
+      <div className={`${styles.padding} bg-tertiary rounded-2xl min-h-[300px]`}>
+        <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>
+            What others say
+          </p>
+          <h2 className={styles.sectionHeadText}>
+            Testimonials.
+          </h2>
+        </motion.div>
+      </div>
+      <div className={`${styles.paddingX} -mt-20 pd-14 flex flex-wrap gap-7`}>
+        {/* // карточки отзывов */}
+        {testimonials.map((testimonial, index) => (
+          <FeedbackCard 
+            key={testimonial.name}
+            index={index}
+            {...testimonial}
+          />
+        ))}
+      </div>
+    </div>
   )
 }
 
-export default Feedbacks
+export default SectionWrapper( Feedbacks, "" ); 
