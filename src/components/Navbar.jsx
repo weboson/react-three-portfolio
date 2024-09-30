1bc41d4514bee527649873fc4a5c3de4a320c910
Rishat <virisound@gmail.com>
2024-09-30 19:06:58 +0500

    Deply to GitHub Pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
-          to="/"
+          to="/react-three-portfolio"
           className="flex items-center gap-2"
           onClick={() => {
             setActive("");
